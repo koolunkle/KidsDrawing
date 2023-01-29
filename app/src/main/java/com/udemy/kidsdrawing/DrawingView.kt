7c54cafbca83fe9ccfdbafa0a959a0609068666c
koolunkle <a43361904@gmail.com>
2023-01-29 15:48:21 +0900

    BrushSizeSelector 준비 및 DisplayMetrics 사용

--- app/src/main/java/com/udemy/kidsdrawing/DrawingView.kt
+++ app/src/main/java/com/udemy/kidsdrawing/DrawingView.kt
@@ -3,6 +3,7 @@ package com.udemy.kidsdrawing
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -36,7 +37,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        // mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -49,7 +50,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         super.onDraw(canvas)
         canvas?.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
 
-        for(path in mPaths) {
+        for (path in mPaths) {
             mDrawPaint!!.strokeWidth = path.brushThickness
             mDrawPaint!!.color = path.color
             canvas?.drawPath(path, mDrawPaint!!)
@@ -93,6 +94,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         }
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
