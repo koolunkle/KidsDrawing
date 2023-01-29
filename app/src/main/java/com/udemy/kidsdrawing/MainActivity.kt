831e89b28be3bc01d308b5e6e68bc28176efef51
koolunkle <a43361904@gmail.com>
2023-01-29 16:38:13 +0900

    커스텀 다이얼로그 통해 브러쉬 사이즈 선택

--- app/src/main/java/com/udemy/kidsdrawing/MainActivity.kt
+++ app/src/main/java/com/udemy/kidsdrawing/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.udemy.kidsdrawing
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val brush: ImageButton = findViewById(R.id.ib_brush)
+        brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+
+        val smallBrush: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBrush.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBrush: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBrush.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBrush: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBrush.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
